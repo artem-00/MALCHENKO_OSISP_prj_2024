eb3fcb557e8e68bbfb7cb4a68c2a06225e2742c7
artem-00 <112750639+artem-00@users.noreply.github.com>
2024-04-24 04:20:18 +0300

    Add files via upload
    finish cpp

---
+++ KP/similar_images.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <filesystem>
+#include <opencv2/opencv.hpp>
+
+namespace fs = std::filesystem;
+
+// Функция для сравнения изображений
+double compareImages(const cv::Mat& image1, const cv::Mat& image2) {
+    cv::Mat hist1, hist2;
+    cv::cvtColor(image1, hist1, cv::COLOR_BGR2HSV);
+    cv::cvtColor(image2, hist2, cv::COLOR_BGR2HSV);
+
+    // Вычисление гистограммы изображений
+    int histSize = 256;
+    float hRanges[] = { 0, 180 };
+    const float* ranges[] = { hRanges };
+    int channels[] = { 0 };
+    cv::MatND histImage1, histImage2;
+    cv::calcHist(&hist1, 1, channels, cv::Mat(), histImage1, 1, &histSize, ranges, true, false);
+    cv::calcHist(&hist2, 1, channels, cv::Mat(), histImage2, 1, &histSize, ranges, true, false);
+
+    // Нормализация гистограмм
+    cv::normalize(histImage1, histImage1, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    cv::normalize(histImage2, histImage2, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+
+    // Вычисление коэффициента схожести (нормализованное расстояние Чебышева)
+    double similarity = cv::compareHist(histImage1, histImage2, cv::HISTCMP_BHATTACHARYYA);
+
+    return similarity;
+}
+
+// Функция для поиска и обработки похожих изображений
+void findSimilarImages(const std::string& folderPath) {
+    // Проверка существования пути к папке
+    if (!fs::exists(folderPath)) {
+        std::cout << "Folder path does not exist." << std::endl;
+        return;
+    }
+
+    // Обход папки и поиск изображений
+    std::vector<cv::Mat> loadedImages;
+    std::vector<std::string> imagePaths;
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        const std::string filePath = entry.path().string();
+
+        // Проверка, является ли файл изображением
+        // Если файл является изображением, загрузите его с помощью OpenCV и обработать
+        if (fs::is_regular_file(entry) && (entry.path().extension() == ".jpg" || entry.path().extension() == ".png" || entry.path().extension() == ".jpeg")) {
+            cv::Mat image = cv::imread(filePath);
+            if (!image.empty()) {
+                loadedImages.push_back(image);
+                imagePaths.push_back(filePath);
+            }
+        }
+    }
+
+    const int numImages = loadedImages.size();
+
+    if (numImages == 0) {
+        std::cout << "No images found in the directory." << std::endl;
+        return;
+    }
+
+    // Выполнение сравнения изображения с другими ранее найденными изображениями  и вывод процента схожести
+    for (int i = 0; i < numImages; ++i) {
+        for (int j = i + 1; j < numImages; ++j) {
+            double similarity = compareImages(loadedImages[i], loadedImages[j]);
+            std::cout << "Similarity between " << imagePaths[i] << " and " << imagePaths[j] << ": " << (1 - similarity) * 100 << "%" << std::endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./image_similarity <folder_path>" << std::endl;
+        return 1;
+    }
+    //g++ -g /home/artem/Documents/KP/similar_images.cpp -o /home/artem/Documents/KP/similar_images `pkg-config --cflags --libs opencv4`
+    std::string folderPath = argv[1];
+    findSimilarImages(folderPath);
+
+    return 0;
+}
